87601a912c50f74ea7e0c2b249bdb862664e5054
MERCYCHEROP <mercy.cherop@student.moringaschool.com>
2026-04-13 16:00:11 +0300

    edited

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,7 +1,23 @@
 // Write your code here
+const products = ['Laptop', 'Smartphone', 'Tablet', 'Headphones', 'Smartwatch'];
 
+function logFirstProduct() {
+  console.log(products[0]);
+}
 
+function addProduct(product) {
+  products.push(product);
+}   
 
+function updateProductName(index, newName) {
+  if (index >= 0 && index < products.length) {
+    products[index] = newName;  
+  }
+}
+
+function removeLastProduct() {
+  products.pop();
+}
 // Export the necessary parts for testing
 module.exports = {
   logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
